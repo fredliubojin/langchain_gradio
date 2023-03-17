bc5ec46a2eb5901593bba8eea00dcd14d4d09951
Fred Liu <fredliu@mbp.local>
2023-03-16 22:28:34 -0700

    Remove unnessisary boxes.

--- chatbot.py
+++ chatbot.py
@@ -20,19 +20,18 @@ def chatbot(api_key, user_input, conversation_history=""):
     set_api_key(api_key)
     response = generate_response(user_input)
     updated_conversation = f"{conversation_history}\nUser: {user_input}\nChatbot: {response}\n"
-    return updated_conversation, response
+    return updated_conversation
 
 api_key_input = gr.inputs.Textbox(lines=1, label="Enter OpenAI API Key")
+
 user_input = gr.inputs.Textbox(lines=3, label="Enter your message")
-conversation_history = gr.inputs.Textbox(lines=10, label="Conversation History")
 
 output_history = gr.outputs.Textbox(label="Updated Conversation")
-output_response = gr.outputs.Textbox(label="Chatbot Response")
 
 iface = gr.Interface(
     fn=chatbot,
-    inputs=[api_key_input, user_input, conversation_history],
-    outputs=[output_history, output_response],
+    inputs=[api_key_input, user_input],
+    outputs=[output_history],
     title="GPT-4 Chatbot",
     description="A simple chatbot using GPT-4 and Gradio with conversation history",
 )
